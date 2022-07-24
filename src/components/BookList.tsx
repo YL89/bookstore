011a9669b88e4b9173effdb3350333622a869148
Yuhao Lu <46984873+YL89@users.noreply.github.com>
2022-07-24 16:58:19 -0400

    feat: optimized booklist content shift

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -15,18 +15,13 @@ const BookList: FC = () => {
   }, []);
 
   return useMemo(() => {
-    if (loading) {
-      return (
-        <div className="w-full h-full">
-          <h2 className="m-auto">Loading</h2>
-        </div>
-      );
-    }
     return (
       <div className="w-full justify-center grow overflow-y-auto grid md:grid-cols-book-4 xs:grid-cols-book-1 sm:grid-cols-book-2 xl:grid-cols-book-8 gap-4 p-5 auto-rows-book ">
-        {books.map((book) => (
-          <BookItem key={book.id} book={book} pClassName={pClassName} />
-        ))}
+        {loading ? (
+          <h2 className="m-auto">Loading</h2>
+        ) : (
+          books.map((book) => <BookItem key={book.id} book={book} pClassName={pClassName} />)
+        )}
       </div>
     );
   }, [books]);
